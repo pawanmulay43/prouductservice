b10c9e20e04ff0e063059da0e2aeb2aee3cb3ce8
Pawan Mulay <Pawan.Mulay@gmail.com>
2023-02-04 18:22:20 +0530

    Initial commit

---
+++ src/main/java/com/techno/prouductservice/ProuductserviceApplication.java
@@ -0,0 +1,13 @@
+package com.techno.prouductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProuductserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProuductserviceApplication.class, args);
+	}
+
+}
